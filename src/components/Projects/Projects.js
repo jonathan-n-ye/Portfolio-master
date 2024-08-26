0e708b4ca8707181eff94c7e63358c00d5ab4844
Jonathan Ye <137107704+jonathan-n-ye@users.noreply.github.com>
2024-08-26 16:15:48 -0400

    Edits

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import cff from "../../Assets/Projects/cff.png";
+import cyis from "../../Assets/Projects/emotion.png";
+import fbla from "../../Assets/Projects/fbla.png";
+import aces from "../../Assets/Projects/chatify.png";
+import wlmacbc from "../../Assets/Projects/suicide.png";
+import cyc from "../../Assets/Projects/blog.png";
 
 function Projects() {
   return (
@@ -23,7 +23,7 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={cff}
               isBlog={false}
               title="Canada FutureFocus"
               description="A podcast serving as a platform for youth to talk about the ideas and issues they care about. Founded March 2023 and serving as Executive Director there."
@@ -34,7 +34,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={cyis}
               isBlog={false}
               title="Canadian Young Investors Society"
               description="Serving as a Senior Director of Finance there. CYIS is dedicated to promoting youth financial literacy and is the largest finance pre-professional club in Canada."
@@ -45,7 +45,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={aces}
               isBlog={false}
               title="ACES"
               description="Multinational organization dedicated to promoting financial literacy, business and STEM for high school students. Serving there as a national leader where I raise and mentor branches."
@@ -56,7 +56,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={fbla}
               isBlog={false}
               title="Canada FBLA"
               description="Worked on the National Advisory Council and helped Canada FBLA expand to 1800+ members. Ran multiple events/panels featuring business professionals and Ivy League students."
@@ -67,7 +67,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={wlmacbc}
               isBlog={false}
               title="William Lyon Mackenzie Business Council"
               description="Serving as the President of the largest chapter of Target Alpha and Canada FBLA. Over 100+ members."
@@ -78,7 +78,7 @@ function Projects() {
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={cyc}
               isBlog={false}
               title="Markham-Thornhill Consituency Youth Council"
               description="Collaborating with Minister Mary Ng and fellow council members to amplify youth voices in the community."
